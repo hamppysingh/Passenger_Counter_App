e06c84818ff50a44f0a1e4bf3695de6aa949536a
hamppysingh <praveenk012340@gmail.com>
2023-11-24 14:49:15 +0530

    FIXED BUG ON RESET BUTTON

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.getElementById("box").addEventListener("mouseenter", function() { //mouseover not working here
     document.getElementById("box").style.transition = "transform 0.3s ease-in-out";
 });
-var p=0;
+var total=0;
 document.getElementById("increment").addEventListener("click", function() {
     var a = parseInt(document.getElementById("inc").innerHTML);
     a += 1;
@@ -10,12 +10,13 @@ document.getElementById("increment").addEventListener("click", function() {
 document.getElementById("reset").addEventListener("click", function() {
     document.getElementById("inc").innerHTML=0;
     document.getElementById("total").innerHTML="";
+    total=0;
     document.getElementById("ans").innerHTML="";
 });
 document.getElementById("save").addEventListener("click", function() {
     var a = parseInt(document.getElementById("inc").innerHTML);
     document.getElementById("ans").innerHTML += a+" - ";
     document.getElementById("inc").innerHTML=0;
-    p+=a;
-    document.getElementById("total").innerHTML=p;
+    total+=a;
+    document.getElementById("total").innerHTML=total;
 });
